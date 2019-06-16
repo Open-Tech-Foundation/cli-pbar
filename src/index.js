43be4aeedb068c83c96b4e237b44f9064bb629a6
Thanga Ganapathy <ganapathy888@gmail.com>
2019-06-16 13:40:54 +0530

    Add progress bar with run & stop methods

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
-export default () => {
-  return "Hello world!";
-};
+const colors = require('./colors');
+const { percentage, percentageVal } = require('./math');
+
+class ProgressBar {
+  constructor(options = {}) {
+    this.width = 30;
+    this.stream = options.stream || process.stderr;
+    this.barChar = options.barChar || '\u2588';
+  }
+
+  render(progress) {
+    this.stream.cursorTo(0);
+    this.stream.write(progress);
+    this.stream.clearLine(1);
+  }
+
+  bar(percent) {
+    const completedWidth = percentageVal(this.width, percent);
+    const remainingWidth = this.width - completedWidth;
+    const completedBar = `${colors.green}${this.barChar}`.repeat(
+      completedWidth
+    );
+    const remainingBar = `${colors.white}${this.barChar}`.repeat(
+      remainingWidth
+    );
+    return completedBar + remainingBar;
+  }
+
+  run(action = '', curVal = 0, totalVal = 0, statusText = '') {
+    const percent = percentage(curVal, totalVal);
+    const barString = this.bar(percent);
+    this.render(
+      `${action} ${barString} ${colors.reset} ${percent}% ${statusText}`
+    );
+  }
+
+  stop(clear = false) {
+    if (clear) {
+      this.stream.cursorTo(0);
+      this.stream.clearLine(1);
+    } else {
+      this.stream.write('\n');
+    }
+  }
+}
+
+module.exports = ProgressBar;
