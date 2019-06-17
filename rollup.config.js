997e109f35906990ed4acb464e5dbb6592d361c2
Thanga Ganapathy <ganapathy888@gmail.com>
2019-06-18 02:05:58 +0530

    Fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,16 @@
-import filesize from "rollup-plugin-filesize";
+import filesize from 'rollup-plugin-filesize';
+import { eslint } from 'rollup-plugin-eslint';
 
-import pkg from "./package.json";
+import pkg from './package.json';
 
 export default [
   // CommonJS (for Node) and ES module (for bundlers) build.
   {
-    input: "src/index.js",
+    input: 'src/index.js',
     output: [
-      { file: `dist/${pkg.main}`, format: "cjs" },
-      { file: `dist/${pkg.module}`, format: "es" }
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
     ],
-    plugins: [filesize()]
-  }
+    plugins: [filesize(), eslint({ throwOnError: true })],
+  },
 ];
